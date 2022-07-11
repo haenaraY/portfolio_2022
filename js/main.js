23e82fc8f34e7c2e33d170a584cea5ff27b90c9d
haenaraY <hnr5034@naver.com>
2022-07-11 22:51:10 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -1,30 +1,13 @@
-//resize
-let vh = window.innerHeight * 0.01
-document.documentElement.style.setProperty('--vh', `${vh}px`)
-window.addEventListener('resize', () => {
-  let vh = window.innerHeight * 0.01
-  document.documentElement.style.setProperty('--vh', `${vh}px`)
-})
-//loading
-let Isloader = document.querySelector('.loading_wrap');
 
-function Isloading(){
-    setTimeout(function(){
-        Isloader.classList.add('active');
-    },2000);
-}
-Isloading();
+//cover
+setTimeout(function () {
 
+    document
+        .getElementById("cover")
+        .classList
+        .add("cover-left");
+});
 
-//앵커기능 부드럽게 스크롤
-document.querySelectorAll(".nav_wrap ul li a").forEach(li => {
-    li.addEventListener("click", e => {
-        e.preventDefault();
-        document.querySelector(li.getAttribute("href")).scrollIntoView({
-            behavior: "smooth"
-        })
-    })
-})
 
 // cursor
 $(document).mousemove(function (e) {
@@ -38,7 +21,7 @@ $(document).mousemove(function (e) {
     });
 });
 
-//마우스 오버효과
+//mouse pointer hover effect
 $(".hoverEffect").hover(function () {
     $(".cursor").addClass("active");
 
@@ -46,14 +29,107 @@ $(".hoverEffect").hover(function () {
     $(".cursor").removeClass("active");
 
 });
+// progress
+const progressTag = document.querySelector('.progress');
+const progressText = document.querySelector('p');
+const bodyTag = document.querySelector('body');
 
-                 
+document.addEventListener('scroll', () => {
+    const pixels = window.pageYOffset;
+    const pageHeight = bodyTag
+        .getBoundingClientRect()
+        .height;
+    const totalHeight = pageHeight - window.innerHeight;
+    const percentage = pixels / totalHeight;
 
-
- setTimeout(function () {
-
-    document.getElementById("cover").classList.add("cover-left");
+    progressTag.style.width = `${ 100 * percentage}%`;
 
 });
+// header sticky
+var win = $(window),
+    nav = $('.scroll_ani');
 
+pos = 10,
+sticky = function () {
+    win.scrollTop() > pos
+        ? nav.addClass('sticky')
+        : nav.removeClass('sticky');
 
+}
+
+// gsap text
+win.scroll(sticky)
+
+let viewHeight = window.innerHeight;
+let viewWidth = window.innerWidth;
+
+let textContainers = document.querySelectorAll('.headline_txt');
+
+textContainers.forEach((element, index) => {
+    let top = element
+        .getBoundingClientRect()
+        .top;
+    let start = top;
+
+    let firstText = element.querySelector('.heading:first-child');
+    let secondText = element.querySelector('.heading:nth-child(2)');
+    let lastText = element.querySelector('.heading:last-child');
+
+    gsap.to(firstText, {
+        scrollTrigger: {
+            trigger: element,
+            duration: 0.8,
+            scrub: 5,
+            start: start + "px bottom",
+            end: "bottom top"
+        },
+        x: '-60vw',
+        transformOrigin: "left center",
+        ease: 'power1.inOut'
+    });
+
+    gsap.to(secondText, {
+        scrollTrigger: {
+            trigger: element,
+            duration: 0.8,
+            scrub: 2,
+            start: start + "px bottom",
+            end: "bottom top"
+        },
+        x: '-5vw',
+        transformOrigin: "left center",
+        ease: 'power1.inOut'
+    });
+
+    gsap.to(lastText, {
+        scrollTrigger: {
+            trigger: element,
+            duration: 0.8,
+            scrub: 3,
+            start: start + "px bottom",
+            end: "bottom top"
+        },
+        x: '50vw',
+        transformOrigin: "left center",
+        ease: 'power1.inOut'
+    });
+});
+
+// parallax
+$(window).scroll(function () {
+    var top = $(window).scrollTop();
+    var scrollTop = $(window).scrollTop() + $(window).height() / 1;
+    for (var i = 1; i <= 10; i++) {
+        if (scrollTop > $(".parallax_item" + i).offset().top) {
+            $(".parallax_item" + i).addClass("show");
+        } else {
+            $(".parallax_item" + i).removeClass("show");
+        }
+    }
+    if (top > 0) {
+        $(".icon-scroll").addClass('hide');
+    } else {
+        $(".icon-scroll").removeClass('hide');
+    }
+
+});
--- js/scroll.js
+++ js/scroll.js
@@ -1,99 +1,10 @@
-// progress
-const progressTag = document.querySelector('.progress');
-const progressText = document.querySelector('p');
-const bodyTag = document.querySelector('body');
 
-document.addEventListener('scroll', () => {
-    const pixels = window.pageYOffset;
-    const pageHeight = bodyTag.getBoundingClientRect().height;
-    const totalHeight = pageHeight - window.innerHeight;
-    const percentage = pixels / totalHeight;
-
-    progressTag.style.width = `${100 * percentage}%`;
-
-  
-  
-});
-// header sticky
-var win = $(window),
-  nav = $('.scroll_ani');
-
-    pos = 10,
-    sticky = function(){ 
-      win.scrollTop() > pos ?
-        nav.addClass('sticky')
-        : nav.removeClass('sticky');
-
-    }
-
-win.scroll(sticky)
-
-let viewHeight = window.innerHeight;
-let viewWidth = window.innerWidth;
-
-let textContainers = document.querySelectorAll('.headline_txt');
-
-textContainers.forEach((element, index) => {
-  let top = element.getBoundingClientRect().top;
-  let start = top ;
-
-  let firstText = element.querySelector('.heading:first-child');
-  let secondText = element.querySelector('.heading:nth-child(2)');
-  let lastText = element.querySelector('.heading:last-child');
-
-  gsap.to(firstText, {
-    scrollTrigger: {
-      trigger: element,
-      duration:0.8,
-      scrub: 5,
-      start: start + "px bottom",
-      end: "bottom top"
-    },
-    x: '-60vw',
-    transformOrigin: "left center", 
-   ease:'power1.inOut'
-  });
-
-    gsap.to(secondText, {
-    scrollTrigger: {
-      trigger: element,
-      duration:0.8,
-      scrub: 2,
-      start: start + "px bottom",
-      end: "bottom top"
-    },
-    x: '-5vw',
-    transformOrigin: "left center", 
-   ease:'power1.inOut'
-    });
-  
-  gsap.to(lastText, {
-    scrollTrigger: {
-      trigger: element,
-      duration:0.8,
-      scrub: 3,
-      start: start + "px bottom",
-      end: "bottom top"
-    },
-    x: '50vw',
-    transformOrigin: "left center", 
-    ease:'power1.inOut'
-  });
-});
-
-// 패럴럭스
-$(window).scroll(function () {
-    var top = $(window).scrollTop();
-    var scrollTop = $(window).scrollTop() + $(window).height() / 1;
-    for (var i = 1; i <= 10; i++) {
-        if (scrollTop > $(".parallax_item" + i).offset().top) {
-            $(".parallax_item" + i).addClass("show");
-        } else {
-            $(".parallax_item" + i).removeClass("show");
-        }
-    }
-    if (top > 0) {     $(".icon-scroll").addClass('hide'); } else {
-    $(".icon-scroll").removeClass('hide'); }
-
-});
-
+//anchor effect
+document.querySelectorAll(".nav_wrap ul li a").forEach(li => {
+    li.addEventListener("click", e => {
+        e.preventDefault();
+        document.querySelector(li.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        })
+    })
+})
